0d82b744e432907be2f2de0078bb5c123a4068ed
blackraydev <black2001ray@mail.ru>
2023-06-15 22:39:58 +0300

    chore: commented

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const Sequilize = require('sequelize');
+// const Sequilize = require('sequelize');
 
-module.exports = new Sequilize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  operatorsAliases: 0,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 3000,
-    idle: 10000,
-  },
-});
+// module.exports = new Sequilize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT,
+//   operatorsAliases: 0,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 3000,
+//     idle: 10000,
+//   },
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userRouter = require('./userRoutes');
+// const userRouter = require('./userRoutes');
 const roomRouter = require('./roomRoutes');
 
 const router = express.Router();
